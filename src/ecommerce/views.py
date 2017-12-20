4c5ac25bed142891c4f9d41cac5bf7d862e053e0
Napat Rungruangbangchan <napat.joe@gmail.com>
2017-12-20 19:40:44 +0700

    14 Basic HTML form at /contact/, GET, POST, using django csrf_token

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -2,7 +2,30 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-	return render(request, "home_page.html", {})
+	context = {
+		"title": "Hello World!",
+		"content": "Welcom to the home_page."
+	}
+	return render(request, "home_page.html", context)
+
+def about_page(request):
+	context = {
+		"title": "About page",
+		"content": "Welcom to the about_page."
+	}
+	return render(request, "home_page.html", context)
+
+def contact_page(request):
+	context = {
+		"title": "Contact page",
+		"content": "Welcom to the contact_page."
+	}
+	if request.method == "POST":
+		print(request.POST)
+		print(request.POST.get("fullname"))
+		print(request.POST.get("email"))
+		print(request.POST.get("content"))
+	return render(request, "contact/view.html", context)
 
 def home_page_old(request):
 	html_ = """
